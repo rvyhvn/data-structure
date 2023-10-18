bd9942cfd55082101baa8052840a056d914ede4f
Hanshi <hanshitrash00@proton.me>
2023-10-18 19:37:34 +0800

    refactor(array generator): make indonesian words to english

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 def print_random_list():
     random_list = [random.randint(1, 100) for _ in range(100)]
-    print("List Angka Acak:")
+    print("Random Number:")
     print(random_list)
 
 def print_nearly_sorted_list():
@@ -11,12 +11,12 @@ def print_nearly_sorted_list():
     for _ in range(30):
         idx1, idx2 = random.randint(0, 99), random.randint(0, 99)
         nearly_sorted_list[idx1], nearly_sorted_list[idx2] = nearly_sorted_list[idx2], nearly_sorted_list[idx1]
-    print("List Angka Hampir Terurut:")
+    print("Nearly Sorted Array:")
     print(nearly_sorted_list)
 
 def print_reversed_list():
     reversed_list = [i for i in range(100, 0, -1)]
-    print("List Angka Terbalik:")
+    print("Reversed Array:")
     print(reversed_list)
 
 print_random_list()
