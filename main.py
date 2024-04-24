8cc7394251041689dfab701bff560c06b610f562
Hanshi <hanshitrash00@proton.me>
2024-04-24 11:18:01 +0800

    Bikin file test.txt

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import random
- 
+
 # Fungsi untuk mencetak list angka acak
+
+
 def print_random_list():
-    random_list = [random.randint(1, 100) for _ in range(100)]
+    random_list = [random.randint(1, 10000) for _ in range(10000)]
     print("List Angka Acak:")
     print(random_list)
- 
+
 # Fungsi untuk mencetak list angka yang hampir terurut
+
+
 def print_nearly_sorted_list():
-    nearly_sorted_list = [i for i in range(1, 101)]
+    nearly_sorted_list = [i for i in range(1, 10001)]
     # Mengacak beberapa elemen untuk membuatnya hampir terurut
     for _ in range(30):
-        idx1, idx2 = random.randint(0, 99), random.randint(0, 99)
+        idx1, idx2 = random.randint(0, 9999), random.randint(0, 9999)
         nearly_sorted_list[idx1], nearly_sorted_list[idx2] = nearly_sorted_list[idx2], nearly_sorted_list[idx1]
     print("List Angka Hampir Terurut:")
     print(nearly_sorted_list)
- 
+
 # Fungsi untuk mencetak list angka terbalik
+
+
 def print_reversed_list():
-    reversed_list = [i for i in range(100, 0, -1)]
+    reversed_list = [i for i in range(10000, 0, -1)]
     print("List Angka Terbalik:")
     print(reversed_list)
- 
+
+
 # Panggil fungsi-fungsi untuk mencetak list
 print_random_list()
 print("\n")
